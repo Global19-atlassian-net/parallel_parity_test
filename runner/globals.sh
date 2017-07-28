da2897321eba4e6b5cb2182adf41ddf30f29d309
Stephan Buys <hello@stephanbuys.com>
2017-07-28 23:02:22 +0200

    Remove rendered file

--- runner/globals.sh
+++
@@ -1,6 +0,0 @@
-PROJECT="ouroboros"
-NODES="ouroboros-node0 ouroboros-node1 ouroboros-node2 ouroboros-node3"
-AWS_DEFAULT_REGION=us-east-1
-PARALLEL_CONCURRENCY=4
-PATH=$PATH:/root/.cargo/bin:/usr/local/bin
-OFFSET=300
