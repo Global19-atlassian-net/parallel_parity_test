3d5802070fa1ef2f6d67f16379709fd3a7597563
Stephan Buys <hello@stephanbuys.com>
2017-07-28 23:12:41 +0200

    Add docker-compose

--- runner/deploy_parity.sh
+++ runner/deploy_parity.sh
@@ -9,6 +9,11 @@ cd $DIR
 export STARTTIME=$(printf "%x" $(($(date +%s) + $OFFSET)))
 mini
 
-cd $DIR/parity docker-compose -p $PROJECT up -d
+cd $DIR/parity 
+parallel -v -j $PARALLEL_CONCURRENCY \
+  "$CMD" ::: $NODES
+
+
+docker-compose -p $PROJECT up -d
 
 #$DIR/docker_cmd.sh build -t parity -f parity/Dockerfile.run parity/
