98f8a046c8e435f003c11626b288dff84e013b77
Stephan Buys <hello@stephanbuys.com>
2017-07-28 21:08:21 +0200

    Fix permissions and syntax errors

--- runner/build_parity.sh
+++ runner/build_parity.sh
@@ -7,6 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd parity
 docker build -t parity_builder .
 mkdir -p out
-docker run --exec --rm --name parity_builder parity_builder
+docker run --rm --name parity_builder parity_builder
 
 docker build -f Dockerfile.run -t parity .
