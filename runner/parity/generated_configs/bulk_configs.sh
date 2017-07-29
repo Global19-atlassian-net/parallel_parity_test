e0db139f4b3e46f9043c8e811d15215dbe084eb2
Stephan Buys <hello@stephanbuys.com>
2017-07-29 16:34:34 +0200

    Also gen passwords

---
+++ runner/parity/generated_configs/bulk_configs.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+mkdir -p ouroboros-node0
+
+cat > ouroboros-node0/config.toml << EOF
+[parity]
+chain = "chain-spec.json"
+base_path = "node"
+
+[network]
+port = 30300
+
+[rpc]
+port = 8540
+apis = ["web3", "eth", "net", "personal", "parity", "parity_set", "traces", "rpc", "parity_accounts"]
+interface = "all"
+hosts = ["all"]
+
+[ui]
+port = 8180
+
+[dapps]
+port = 8080
+
+[ipc]
+disable = true
+
+[account]
+password = ["passwords"]
+
+[mining]
+engine_signer = "0x00bd138abd70e2f00903268f3db08f2d25677c9e"
+reseal_on_txs = "none"
+usd_per_tx = "0"
+EOF
+
+cat > ouroboros-node0/stakeholdersetup.json << EOF
+{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["node0", "node0"],"id":0}
+EOF
+
+
+mkdir -p ouroboros-node1
+
+cat > ouroboros-node1/config.toml << EOF
+[parity]
+chain = "chain-spec.json"
+base_path = "node"
+
+[network]
+port = 30300
+
+[rpc]
+port = 8540
+apis = ["web3", "eth", "net", "personal", "parity", "parity_set", "traces", "rpc", "parity_accounts"]
+interface = "all"
+hosts = ["all"]
+
+[ui]
+port = 8180
+
+[dapps]
+port = 8080
+
+[ipc]
+disable = true
+
+[account]
+password = ["passwords"]
+
+[mining]
+engine_signer = "0x00aa39d30f0d20ff03a22ccfc30b7efbfca597c2"
+reseal_on_txs = "none"
+usd_per_tx = "0"
+EOF
+
+cat > ouroboros-node1/stakeholdersetup.json << EOF
+{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["node1", "node1"],"id":0}
+EOF
+
