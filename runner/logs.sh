e73cb0276b625f31b687c0ef2eebbc8c04f30a8d
Stephan Buys <hello@stephanbuys.com>
2017-07-29 22:49:19 +0200

    add log viewer

--- runner/logs.sh
+++ runner/logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ./globals.sh
 
-docker $(docker-machine config $PROJECT-node${1}) logs parity_parity_1 "$@"
+docker $(docker-machine config $PROJECT-node${1}) logs parity_parity_1 ${2}
