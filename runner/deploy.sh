67fcc5c6924402c7bd9d68afc31ffc5325b34c52
Stephan Buys <hello@stephanbuys.com>
2017-07-29 22:30:59 +0200

    Fix paths

--- runner/deploy.sh
+++ runner/deploy.sh
@@ -2,19 +2,22 @@
 
 set -e
 
-deploy_1_build_and_clear.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+./deploy_1_build_and_clear.sh
 sleep 5
 
-deploy_2_run_initialise.sh
+./deploy_2_run_initialise.sh
 sleep 5
 
-deploy_3_configure_users.sh
+./deploy_3_configure_users.sh
 sleep 5
 
-deploy_4_production_mode.sh
+./deploy_4_production_mode.sh
 sleep 5
 
-deploy_5_notify_nodes.sh.orig.tpl
+./deploy_5_notify_nodes.sh.orig.tpl
 sleep 5
 
-deploy_6_transactions.sh
+./deploy_6_transactions.sh
