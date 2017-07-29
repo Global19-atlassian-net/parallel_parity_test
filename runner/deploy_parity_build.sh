44008c50a7d8a8992881de20545a729426496213
Stephan Buys <hello@stephanbuys.com>
2017-07-29 16:52:08 +0200

    Small refactor

--- runner/deploy_parity_build.sh
+++ runner/deploy_parity_build.sh
@@ -7,8 +7,5 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-$DIR/docker_compose_cmd.sh -f parity/docker-compose.yml build
-
 export DC_COMMAND="./initial.sh"
 $DIR/docker_compose_cmd.sh -f parity/docker-compose.yml up -d
-
--- runner/deploy_parity_init.sh
+++ runner/deploy_parity_init.sh
@@ -8,9 +8,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 export STARTTIME=$(printf "%x" $(($(date +%s) + $OFFSET)))
+
 mini
+
+cat parity/chain-spec.json
+exit 1
+
 cd $DIR/parity/generated_configs
 chmod u+x ./bulk_configs.sh
 ./bulk_configs.sh
 cd $DIR
 
+$DIR/docker_compose_cmd.sh -f parity/docker-compose.yml build
