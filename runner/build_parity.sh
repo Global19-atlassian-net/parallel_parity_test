69040ec111bfd4f9f2bd7bfd5ba685cdf57251b8
Stephan Buys <hello@stephanbuys.com>
2017-08-06 19:24:58 +0200

    Caching and fixed output

--- runner/build_parity.sh
+++ runner/build_parity.sh
@@ -8,25 +8,35 @@ cd $DIR
 source ./globals.sh
 
 # Verify AWS tokens
-[[  ! -n "$AWS_ACCESS_KEY_ID" || ! -n "AWS_SECRET_ACCESS_KEY" ]] && {
-   echo "Please define the apporpriate AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables."
-   exit 1
-}
+# [[  ! -n "$AWS_ACCESS_KEY_ID" || ! -n "AWS_SECRET_ACCESS_KEY" ]] && {
+#    echo "Please define the apporpriate AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables."
+#    exit 1
+# }
 
-DATETIME=$(date +'%Y%m%d')
+export DATETIME=$(date +'%Y%m%d%H%M')
 
 export AWS_INSTANCE_TYPE=c4.large
 
 set +e 
-docker-machine create --driver amazonec2 builder
+# docker-machine create --driver amazonec2 builder
 set -e
-eval $(docker-machine env --shell=bash builder)
+# eval $(docker-machine env --shell=bash builder)
+
+# cd parity
+# docker build --build-arg DATETIME=${DATETIME} --build-arg RUSTVER=${RUSTVER} -t parity_builder .
+# mkdir -p out
+# set +e
+# docker rm parity_builder
+# sleep 5
+# docker run --name parity_builder parity_builder
+# sleep 10
+# set -e
+# docker cp parity_builder:/parity/target/release/parity ./parity/out/
 
 cd parity
-docker build --build-arg DATETIME=${DATETIME} --build-arg RUSTVER=${RUSTVER} -t parity_builder .
-mkdir -p out
-docker rm parity_builder
-docker run --name parity_builder parity_builder
-docker cp parity_builder:/parity/target/release/parity ./parity/out/
+docker-compose -p parity -f docker-compose-build.yml build --build-arg DATETIME=${DATETIME} --build-arg RUSTVER=${RUSTVER} parity_builder
+docker-compose -p parity -f docker-compose-build.yml up
 
-docker-machine rm -y builder
+docker cp parity_parity_builder_1:/parity/target/release/parity ./out/
+
+# docker-machine rm -y builder
